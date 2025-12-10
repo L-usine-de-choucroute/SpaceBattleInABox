1ba8824f5abcfb814d33ddf47c750dcf2b71507b
Evendred <evendre@gmail.com>
2025-12-10 20:25:57 +0100

    feat(NodeTree): Use relative position for Space node

--- space/space.gd
+++ space/space.gd
@@ -12,6 +12,11 @@ const layer_size = 5
 	$SpaceLayer4,
 ]
 
+func _ready() -> void:
+	var _position = layers[0].get_parent_position_for_cell(Vector2i(0, 0))
+	ship.set_at_position(0, _position)
+	pass
+
 func _on_space_layer_cell_clicked(layer_level: int, cell: Vector2i) -> void:
 	var _position = layers[layer_level].get_parent_position_for_cell(cell)
 	ship.set_at_position(layer_level, _position)
