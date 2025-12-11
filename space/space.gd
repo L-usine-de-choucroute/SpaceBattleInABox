1ea077f2c3db2e9f901f3ff51191919c3ba3bf9b
Evendred <evendre@gmail.com>
2025-12-11 21:00:20 +0100

    feat(Ship): Add 5 ship types

--- space/space.gd
+++ space/space.gd
@@ -3,7 +3,11 @@ extends Node
 class_name Space
 
 const layer_size = 5
-@onready var ship: Ship = $Ship
+@onready var space_base: Ship = $SpaceBase
+@onready var cruiser: Ship = $Cruiser
+@onready var frigate0: Ship = $Frigate0
+@onready var frigate1: Ship = $Frigate1
+@onready var fighter: Ship = $Fighter
 @onready var layers: Array[Space_Layer] = [
 	$SpaceLayer0,
 	$SpaceLayer1,
@@ -13,10 +17,15 @@ const layer_size = 5
 ]
 
 func _ready() -> void:
-	var _position = layers[0].get_parent_position_for_cell(Vector2i(0, 0))
-	ship.set_at_position(0, _position)
-	pass
+	move_ship(space_base, 0, Vector2i(0, 0))
+	move_ship(cruiser, 0, Vector2i(1, 0))
+	move_ship(frigate0, 0, Vector2i(0, 1))
+	move_ship(frigate1, 0, Vector2i(2, 0))
+	move_ship(fighter, 0, Vector2i(0, 2))
 
 func _on_space_layer_cell_clicked(layer_level: int, cell: Vector2i) -> void:
+	move_ship(fighter, layer_level, cell)
+
+func move_ship(ship: Ship, layer_level: int, cell: Vector2i) -> void:
 	var _position = layers[layer_level].get_parent_position_for_cell(cell)
 	ship.set_at_position(layer_level, _position)
