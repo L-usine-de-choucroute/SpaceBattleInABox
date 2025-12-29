928301414d1c3526ac2fb08200ba4ec857d47d60
Evendred <evendre@gmail.com>
2025-12-29 21:11:44 +0100

    feat(Move): Pevent ship to move on another ship

--- space/space.gd
+++ space/space.gd
@@ -57,8 +57,8 @@ func _on_space_layer_cell_clicked(layer_level: int, cell: Vector2i):
 	if (selected_tile == null):
 		select_tile(layer_level, cell)
 	else:
-		move_ship_at_selected_tile(layer_level, cell)
-		unselect_tile()
+		var isMoved = try_move_ship_at_selected_tile(layer_level, cell)
+		if (isMoved): unselect_tile()
 
 func select_tile(target_layer: int, target_cell: Vector2i):
 	selected_tile = Vector3i(target_cell.x, target_cell.y, target_layer)
@@ -69,20 +69,23 @@ func unselect_tile():
 	layers[selected_tile.z].unselect_cell(Vector2i(selected_tile.x, selected_tile.y))
 	selected_tile = null
 
-func move_ship_at_selected_tile(target_layer: int, target_cell: Vector2i):
+func try_move_ship_at_selected_tile(target_layer: int, target_cell: Vector2i):
 	var selected_ship = get_ship_at_selected_tile()
-	if (selected_ship == null): return
-	move_ship(selected_ship, selected_tile, target_layer, target_cell)
+	if (selected_ship == null): return false
+	return try_move_ship(selected_ship, selected_tile, target_layer, target_cell)
 
 func get_ship_at_selected_tile():
 	return ships_location.get_ship(selected_tile.x, selected_tile.y, selected_tile.z)
 
 func add_ship(ship: Ship, target_layer: int, target_cell: Vector2i):
-	move_ship(ship, null, target_layer, target_cell)
+	try_move_ship(ship, null, target_layer, target_cell)
 
-func move_ship(ship: Ship, from, target_layer: int, target_cell: Vector2i):
+func try_move_ship(ship: Ship, from, target_layer: int, target_cell: Vector2i):
+	var target_content = ships_location.get_ship(target_cell.x, target_cell.y, target_layer)
+	if (target_content != null): return false # Prevent override another ship
 	var target_position = layers[target_layer].get_parent_position_for_cell(target_cell)
 	ship.set_at_position(target_layer, target_position)
 	var isFocused = selected_layer == target_layer
 	ship.update_focus(isFocused)
 	ships_location.move_ship(ship, from, Vector3i(target_cell.x, target_cell.y, target_layer))
+	return true
